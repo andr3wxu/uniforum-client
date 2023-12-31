17c5c60a7590dd0147300050a1d415c9e7a724af
Andrew Xu <andr3w.xu@gmail.com>
2023-12-30 22:55:55 -0700

    added data types in definitions.ts for sql queries

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,4 +1,28 @@
-export type product = {
-  product: string,
-  price: number,
+export type user = {
+  user_id: number,
+  username: string,
+  email: string,
+  password_hash: string
 };
+
+export type posts = {
+  user_id: number,
+  post_id: number,
+  p_query: string,
+  // p_time_posted: ?? DATE??
+  p_upvotes: number,
+  category: "" | undefined
+}
+
+export type comments = {
+  comment_id: number,
+  post_id: number,
+  c_query: string,
+  // c_time_posted: ?? DATE??
+  c_upvotes: number,
+}
+
+export type saved_posts = {
+  user_id: number,
+  post_id: number
+}
